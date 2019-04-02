9d1f44c0a2a49d5bd9b5f004aeb67b6430218f50
garance bazan <>
2019-04-02 17:31:54 +0200

    photo

--- app/src/main/java/com/edu/rb/mytracker2/Photos.java
+++ app/src/main/java/com/edu/rb/mytracker2/Photos.java
@@ -2,12 +2,15 @@ package com.edu.rb.mytracker2;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -22,10 +25,13 @@ import java.util.Date;
 public class Photos extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int PICTURE_RESULT = 2;
     static final int REQUEST_TAKE_PHOTO = 1;
     ImageView imageView;
     String currentPhotoPath;
     private Button btnEnregistrer;
+    private Bitmap image;
+    File fileToWriteIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,7 @@ public class Photos extends AppCompatActivity {
         btnEnregistrer=(Button)findViewById(R.id.enregistrer);
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
         startActivityForResult(intent, 0);
 
         createOnClickBtnEnregistrer();
@@ -46,7 +53,28 @@ public class Photos extends AppCompatActivity {
         btnEnregistrer.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MediaStore.Images.Media.insertImage(getContentResolver(), imageView, "nom image", "description");
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                //création dossier et sous-dossier
+                File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "Match Ping-Pong");
+                //if it doesn't exist the folder will be created
+                if (!file.exists()) {
+                    file.mkdirs();
+                }
+                File folder = new File(file, "match ");
+                if (!folder.exists()) {
+                    folder.mkdirs();
+                }
+
+                //creation fichier
+                String fileName = "photo_" + new SimpleDateFormat("yyMMddHHmmss").format(new Date()) + ".jpg";
+                fileToWriteIn = new File(folder, fileName);
+                Uri photoPath = FileProvider.getUriForFile(Photos.this, "asynctask.moi.android.fr.ping_pongtracker.provider", fileToWriteIn);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoPath);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                startActivityForResult(intent, PICTURE_RESULT);
             }
         });
 
@@ -59,6 +87,29 @@ public class Photos extends AppCompatActivity {
         Bitmap bit= (Bitmap) data.getExtras().get("data");
         imageView.setImageBitmap(bit);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_navigation_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.home_logo:
+                Intent intentHome = new Intent (this, HomePage.class);
+                startActivity(intentHome);
+                return true;
+            case R.id.camera_logo:
+                Intent intentPhotos = new Intent (this, Photos.class);
+                startActivity(intentPhotos);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -85,6 +136,37 @@ public class Photos extends AppCompatActivity {
         // Save a file: path for use with ACTION_VIEW intents
         currentPhotoPath = image.getAbsolutePath();
         return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 
 }
