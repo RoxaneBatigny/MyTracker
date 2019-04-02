97e80513861fd9f756b48518dfaa6701f25a6109
Roxane <roxane.batigny@edu.ece.fr>
2019-04-02 13:14:42 +0200

    menuok3

--- app/src/main/java/com/edu/rb/mytracker2/HomePage.java
+++ app/src/main/java/com/edu/rb/mytracker2/HomePage.java
@@ -3,6 +3,7 @@ package com.edu.rb.mytracker2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 
@@ -21,6 +22,13 @@ public class HomePage extends AppCompatActivity implements Main_Fragment.OnButto
         getSupportFragmentManager().beginTransaction().add(R.id.frm, main_fragment).commit();
 
         setTitle("g");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_navigation_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     public void IntentNvMatch()
